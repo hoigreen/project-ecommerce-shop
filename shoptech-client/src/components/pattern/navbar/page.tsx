14ab4f426e4119f51fc380c1d807913ec2a90bf6
HoiGreen <hoitrnd107@gmail.com>
2023-09-29 15:23:10 +0700

    complete ui navbar

--- shoptech-client/src/components/pattern/navbar/page.tsx
+++ shoptech-client/src/components/pattern/navbar/page.tsx
@@ -1,20 +1,21 @@
 'use client'
-import React from 'react'
+import React, { useContext } from 'react'
 import Container from '@mui/material/Container';
 import Image from 'next/image'
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { Input } from '@mui/material';
-import { Search } from '@mui/icons-material';
 import { FiSearch } from 'react-icons/fi'
 import { BsCart } from 'react-icons/bs'
 import { FaRegUser } from 'react-icons/fa'
 import { AiOutlineHistory } from 'react-icons/ai'
 import { BiSupport } from 'react-icons/bi'
-import { Button } from "@material-tailwind/react";
+import { StateContext } from '@/contexts/StateContext';
 
 const NavBar = () => {
   const router = useRouter()
+  const { isDropdownAccountNavBarOpen, toggleDropdownAccountNavBar } = useContext(StateContext)
+
+
 
   const handleLoadingPage = (second: number) => {
 
@@ -72,22 +73,36 @@ const NavBar = () => {
                   <p className="header--btn-name" >Giỏ hàng</p>
                 </button>
 
-                <button
-                  data-dropdown-toggle="dropdownNavbar"
-                  className="flex text-sm items-center mx-2 text-white font-semibold bg-green-600 w-fit h-12 px-4 py-2 outline-none border-none cursor-pointer rounded-lg hover:bg-green-500"
-                  onClick={(e) => {
-                    // if (auth.username) {
-                    //   const elementNavOption = document.querySelector('.nav__option-box');
-                    //   elementNavOption.style.display = 'block';
-                    // }
-                    // else {
-                    //   handleLoadingPage(1)
-                    //   window.location.href = "/login"
-                    // }
-                  }}>
-                  <div className="mr-2 text-xl"><FaRegUser /></div>
-                  <p className="header--btn-name">Thành viên</p>
-                </button>
+                <div className="relative inline-block text-left">
+                  <button
+                    className="flex text-sm items-center mx-2 text-white font-semibold bg-green-600 w-fit h-12 px-4 py-2 outline-none border-none cursor-pointer rounded-lg hover:bg-green-500"
+                    onClick={(e) => {
+                      toggleDropdownAccountNavBar()
+                      // if (auth.username) {
+                      //   const elementNavOption = document.querySelector('.nav__option-box');
+                      //   elementNavOption.style.display = 'block';
+                      // }
+                      // else {
+                      //   handleLoadingPage(1)
+                      //   window.location.href = "/login"
+                      // }
+                    }}>
+                    <div className="mr-2 text-xl"><FaRegUser /></div>
+                    <p className="header--btn-name">Thành viên</p>
+                  </button>
+                  {
+                    isDropdownAccountNavBarOpen &&
+                    <div className="absolute right-0 z-10 mt-4 w-40 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <div className="py-1 hover:bg-gray-200 ">
+                        <Link href="#" className="text-gray-700 font-semibold block px-4 py-2 text-sm">Edit</Link>
+                      </div>
+                      <div className="py-1 hover:bg-gray-200 ">
+                        <Link href="#" className="text-gray-700 font-semibold block px-4 py-2 text-sm">Archive</Link>
+                      </div>
+                    </div>
+                  }
+                </div>
+
 
                 <button className="flex text-sm items-center mx-2 text-white font-semibold bg-green-600 w-fit h-12 px-4 py-2 outline-none border-none cursor-pointer rounded-lg hover:bg-green-500" onClick={() => {
                   handleLoadingPage(1); window.location.href = "/order"
@@ -112,26 +127,10 @@ const NavBar = () => {
                 <li className="nav__option-item" style={{ color: "red" }} onClick={e => { }}>Đăng xuất</li>
               </ul> */}
 
-              <div id="dropdownNavbar" className="hidden bg-white text-base z-10 list-none divide-y divide-gray-100 rounded shadow my-4 w-44">
-                <ul className="py-1" aria-labelledby="dropdownLargeButton">
-                  <li>
-                    <a href="#" className="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Dashboard</a>
-                  </li>
-                  <li>
-                    <a href="#" className="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Settings</a>
-                  </li>
-                  <li>
-                    <a href="#" className="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Earnings</a>
-                  </li>
-                </ul>
-                <div className="py-1">
-                  <a href="#" className="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Sign out</a>
-                </div>
-              </div>
             </nav>
           </div>
         </Container>
-      </div>
+      </div >
     </React.Fragment >
   )
 }
--- shoptech-client/src/contexts/StateContext.tsx
+++ shoptech-client/src/contexts/StateContext.tsx
@@ -2,251 +2,224 @@
 import { createContext, useEffect, useMemo, useRef, useState, PropsWithChildren } from "react";
 import { useTranslation } from "react-i18next";
 
-interface State {
-  isSidebarOpen: boolean;
-  isDrawerOpen: boolean;
-  isBulkDrawerOpen: boolean;
-  isModalOpen: boolean;
-  isAllocateModalOpen: boolean;
-  isOpportunityModalOpen: boolean;
-  isUpdate: boolean;
-  lang: string;
-  time: string;
-  sortedField: string;
-  typeLead: string;
-  currentPage: number;
-  searchText: string | null;
-  invoice: string | null;
-  zone: string;
-  status: string;
-  category: any;
-  startDate: string;
-  endDate: string;
-  modalOpen: boolean;
-  windowDimension: number;
-  loading: boolean;
-  navBar: boolean;
-  tabIndex: number;
-  queryParams: object;
-  selectedProductFactor: string;
-  isEventClientModalOpen: boolean;
-  isView: boolean;
-}
-
 interface StateContextType {
-  state: State;
-  updateState: (newState: Partial<State>) => void;
+  isDropdownAccountNavBarOpen: boolean;
+  closeDropdownAccountNavBarOpen: () => void,
+  toggleDropdownAccountNavBar: () => void
+  // isSidebarOpen: boolean;
+  // isDrawerOpen: boolean;
+  // isBulkDrawerOpen: boolean;
+  // isModalOpen: boolean;
+  // isAllocateModalOpen: boolean;
+  // isOpportunityModalOpen: boolean;
+  // isUpdate: boolean;
+  // lang: string;
+  // time: string;
+  // sortedField: string;
+  // typeLead: string;
+  // currentPage: number;
+  // searchText: string | null;
+  // invoice: string | null;
+  // zone: string;
+  // status: string;
+  // category: any;
+  // startDate: string;
+  // endDate: string;
+  // modalOpen: boolean;
+  // windowDimension: number;
+  // loading: boolean;
+  // navBar: boolean;
+  // tabIndex: number;
+  // queryParams: object;
+  // selectedProductFactor: string;
+  // isEventClientModalOpen: boolean;
+  // isView: boolean;
 }
 
 // create context
-export const StateContext = createContext<StateContextType | undefined>(undefined);
+export const StateContext = createContext<StateContextType>({} as StateContextType);
 
 export const StateProvider = ({ children }: PropsWithChildren<{}>) => {
-  const initialState: State = {
-    isSidebarOpen: false,
-    isDrawerOpen: false,
-    isBulkDrawerOpen: false,
-    isModalOpen: false,
-    isAllocateModalOpen: false,
-    isOpportunityModalOpen: false,
-    isUpdate: false,
-    lang: "en",
-    time: "",
-    sortedField: "",
-    typeLead: "",
-    currentPage: 1,
-    searchText: null,
-    invoice: null,
-    zone: "",
-    status: "",
-    category: null,
-    startDate: "",
-    endDate: "",
-    modalOpen: false,
-    windowDimension: 0,
-    loading: false,
-    navBar: true,
-    tabIndex: 0,
-    queryParams: {},
-    selectedProductFactor: "",
-    isEventClientModalOpen: false,
-    isView: false,
-  };
-
-  const [state, setState] = useState<State>(initialState);
-
-  const updateState = (newState: Partial<State>) => {
-    setState({ ...state, ...newState });
-  };
-
   const resultsPerPage = 20;
   const searchRef = useRef("");
   const invoiceRef = useRef("");
 
-  const [limitData, setLimitData] = useState(20);
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  const [isBulkDrawerOpen, setIsBulkDrawerOpen] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isAllocateModalOpen, setIsAllocateModalOpen] = useState(false);
-  const [isOpportunityModalOpen, setIsOpportunityModalOpen] = useState(false);
-  const [isUpdate, setIsUpdate] = useState(false);
-  const [lang, setLang] = useState("en");
-  const [time, setTime] = useState("");
-  const [sortedField, setSortedField] = useState("");
-  const [typeLead, setTypeLead] = useState("");
-  const [currentPage, setCurrentPage] = useState(1);
-  const [searchText, setSearchText] = useState(null);
-  const [invoice, setInvoice] = useState(null);
-  const [zone, setZone] = useState("");
-  const [status, setStatus] = useState("");
-  const [category, setCategory] = useState(null);
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
-  const [modalOpen, setModalOpen] = useState(false);
-  const [windowDimension, setWindowDimension] = useState(0);
-  const [loading, setLoading] = useState(false);
-  const [navBar, setNavBar] = useState(true);
-  const { i18n } = useTranslation();
-  const [tabIndex, setTabIndex] = useState(0);
-  const [queryParams, setQueryParams] = useState({});
-  const [selectedProductFactor, setSelectedProductFactor] = useState("");
-  const [isEventClientModalOpen, setIsEventClientModalOpen] = useState(false);
-  const [isView, setIsView] = useState(false);
+  const [isDropdownAccountNavBarOpen, setIsDropdownAccountNavBarOpen] = useState(false)
+  const closeDropdownAccountNavBarOpen = () => setIsDropdownAccountNavBarOpen(false);
+  const toggleDropdownAccountNavBar = () => setIsDropdownAccountNavBarOpen(!isDropdownAccountNavBarOpen);
 
-  const initPopupState = useMemo(
-    () => ({
-      clientEvent: false,
-      clientNote: false,
-      clientClaim: false,
-      clientAgree: false,
-      clientQoutate: false,
-      id: "",
-    }),
-    []
-  );
-  const [isPopupOpen, setIsPopupOpen] = useState(initPopupState);
+  // const [isAddCaseModalOpen, setIsAddCaseModalOpen] = useState(false)
 
-  const closePopup = () => setIsPopupOpen(initPopupState);
+  // const [limitData, setLimitData] = useState(20);
+  // const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  // const [isBulkDrawerOpen, setIsBulkDrawerOpen] = useState(false);
+  // const [isModalOpen, setIsModalOpen] = useState(false);
+  // const [isAllocateModalOpen, setIsAllocateModalOpen] = useState(false);
+  // const [isOpportunityModalOpen, setIsOpportunityModalOpen] = useState(false);
+  // const [isUpdate, setIsUpdate] = useState(false);
+  // const [lang, setLang] = useState("en");
+  // const [time, setTime] = useState("");
+  // const [sortedField, setSortedField] = useState("");
+  // const [typeLead, setTypeLead] = useState("");
+  // const [currentPage, setCurrentPage] = useState(1);
+  // const [searchText, setSearchText] = useState(null);
+  // const [invoice, setInvoice] = useState(null);
+  // const [zone, setZone] = useState("");
+  // const [status, setStatus] = useState("");
+  // const [category, setCategory] = useState(null);
+  // const [startDate, setStartDate] = useState("");
+  // const [endDate, setEndDate] = useState("");
+  // const [modalOpen, setModalOpen] = useState(false);
+  // const [windowDimension, setWindowDimension] = useState(0);
+  // const [loading, setLoading] = useState(false);
+  // const [navBar, setNavBar] = useState(true);
+  // const { i18n } = useTranslation();
+  // const [tabIndex, setTabIndex] = useState(0);
+  // const [queryParams, setQueryParams] = useState({});
+  // const [selectedProductFactor, setSelectedProductFactor] = useState("");
+  // const [isEventClientModalOpen, setIsEventClientModalOpen] = useState(false);
+  // const [isView, setIsView] = useState(false);
+
+  // const initPopupState = useMemo(
+  //   () => ({
+  //     clientEvent: false,
+  //     clientNote: false,
+  //     clientClaim: false,
+  //     clientAgree: false,
+  //     clientQoutate: false,
+  //     id: "",
+  //   }),
+  //   []
+  // );
+  // const [isPopupOpen, setIsPopupOpen] = useState(initPopupState);
+
+  // const closePopup = () => setIsPopupOpen(initPopupState);
 
   // const closeSidebar = () => updateState(isSidebarOpen: false);
   // const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
-  const toggleViewStatus = () => setIsView(!isView);
+  // const toggleViewStatus = () => setIsView(!isView);
 
-  const closeDrawer = () => {
-    setIsView(false)
-    setIsDrawerOpen(false);
-  };
-  const toggleDrawer = () => {
-    setIsDrawerOpen(!isDrawerOpen)
-    setIsView(true)
-  };
+  // const closeDrawer = () => {
+  //   setIsView(false)
+  //   setIsDrawerOpen(false);
+  // };
+  // const toggleDrawer = () => {
+  //   setIsDrawerOpen(!isDrawerOpen)
+  //   setIsView(true)
+  // };
 
-  const closeBulkDrawer = () => setIsBulkDrawerOpen(false);
-  const toggleBulkDrawer = () => setIsBulkDrawerOpen(!isBulkDrawerOpen);
+  // const closeBulkDrawer = () => setIsBulkDrawerOpen(false);
+  // const toggleBulkDrawer = () => setIsBulkDrawerOpen(!isBulkDrawerOpen);
 
-  const closeModal = () => setIsModalOpen(false);
-  const toggleModal = () => setIsModalOpen(!isModalOpen);
+  // const closeModal = () => setIsModalOpen(false);
+  // const toggleModal = () => setIsModalOpen(!isModalOpen);
 
-  const closeAllocateModal = () => setIsAllocateModalOpen(false);
-  const toggleAllocateModal = () =>
-    setIsAllocateModalOpen(!isAllocateModalOpen);
+  // const closeAllocateModal = () => setIsAllocateModalOpen(false);
+  // const toggleAllocateModal = () =>
+  //   setIsAllocateModalOpen(!isAllocateModalOpen);
 
-  const closeOpportunityModal = () => setIsOpportunityModalOpen(false);
-  const toggleOpportunityModal = () =>
-    setIsOpportunityModalOpen(!isOpportunityModalOpen);
+  // const closeOpportunityModal = () => setIsOpportunityModalOpen(false);
+  // const toggleOpportunityModal = () =>
+  //   setIsOpportunityModalOpen(!isOpportunityModalOpen);
 
-  const [isBasicInfoCollapsed, setIsBasicInfoCollapsed] = useState(false);
-  const handleBasicInfoToggle = () =>
-    setIsBasicInfoCollapsed(!isBasicInfoCollapsed);
+  // const [isBasicInfoCollapsed, setIsBasicInfoCollapsed] = useState(false);
+  // const handleBasicInfoToggle = () =>
+  //   setIsBasicInfoCollapsed(!isBasicInfoCollapsed);
 
-  const [isRequestInfoCollapsed, setIsRequestInfoCollapsed] = useState(false);
-  const handleRequestInfoToggle = () =>
-    setIsRequestInfoCollapsed(!isRequestInfoCollapsed);
+  // const [isRequestInfoCollapsed, setIsRequestInfoCollapsed] = useState(false);
+  // const handleRequestInfoToggle = () =>
+  //   setIsRequestInfoCollapsed(!isRequestInfoCollapsed);
 
-  const [isContractInfoCollapsed, setIsContractInfoCollapsed] = useState(false);
-  const handleContractInfoToggle = () =>
-    setIsContractInfoCollapsed(!isContractInfoCollapsed);
+  // const [isContractInfoCollapsed, setIsContractInfoCollapsed] = useState(false);
+  // const handleContractInfoToggle = () =>
+  //   setIsContractInfoCollapsed(!isContractInfoCollapsed);
 
-  const [isAdditionalInfoCollapsed, setIsAdditionalInfoCollapsed] =
-    useState(false);
-  const handleAdditionalInfoToggle = () =>
-    setIsAdditionalInfoCollapsed(!isAdditionalInfoCollapsed);
+  // const [isAdditionalInfoCollapsed, setIsAdditionalInfoCollapsed] =
+  //   useState(false);
+  // const handleAdditionalInfoToggle = () =>
+  //   setIsAdditionalInfoCollapsed(!isAdditionalInfoCollapsed);
 
-  const [isPaymentInfoCollapsed, setIsPaymentInfoCollapsed] = useState(false);
-  const handlePaymentInfoToggle = () =>
-    setIsPaymentInfoCollapsed(!isPaymentInfoCollapsed);
+  // const [isPaymentInfoCollapsed, setIsPaymentInfoCollapsed] = useState(false);
+  // const handlePaymentInfoToggle = () =>
+  //   setIsPaymentInfoCollapsed(!isPaymentInfoCollapsed);
 
-  const [isComponentMounted, setIsComponentMounted] = useState(false);
-  const handleComponentMountedToggle = () =>
-    setIsComponentMounted(!isComponentMounted);
+  // const [isComponentMounted, setIsComponentMounted] = useState(false);
+  // const handleComponentMountedToggle = () =>
+  //   setIsComponentMounted(!isComponentMounted);
 
-  const closeEventClientModal = () => setIsEventClientModalOpen(false);
-  const toggleEventClientModal = () =>
-    setIsEventClientModalOpen(!isEventClientModalOpen);
+  // const closeEventClientModal = () => setIsEventClientModalOpen(false);
+  // const toggleEventClientModal = () =>
+  //   setIsEventClientModalOpen(!isEventClientModalOpen);
 
-  const [isNoChecked, setIsNoChecked] = useState(false);
-  const [isYesChecked, setIsYesChecked] = useState(false);
+  // const [isNoChecked, setIsNoChecked] = useState(false);
+  // const [isYesChecked, setIsYesChecked] = useState(false);
 
-  const [isAddCaseModalOpen, setIsAddCaseModalOpen] = useState(false)
-  const closeAddCaseModal = () => setIsAddCaseModalOpen(false);
-  const toggleAddCaseModal = () => setIsAddCaseModalOpen(!isAddCaseModalOpen);
+  // const [isAddCaseModalOpen, setIsAddCaseModalOpen] = useState(false)
+  // const closeAddCaseModal = () => setIsAddCaseModalOpen(false);
+  // const toggleAddCaseModal = () => setIsAddCaseModalOpen(!isAddCaseModalOpen);
 
-  const [isMarkInvalidModalOpen, setIsMarkInvalidModalOpen] = useState(false)
-  const closeMarkInvalidModal = () => setIsMarkInvalidModalOpen(false);
-  const toggleMarkInvalidModal = () => setIsMarkInvalidModalOpen(!isMarkInvalidModalOpen);
+  // const [isMarkInvalidModalOpen, setIsMarkInvalidModalOpen] = useState(false)
+  // const closeMarkInvalidModal = () => setIsMarkInvalidModalOpen(false);
+  // const toggleMarkInvalidModal = () => setIsMarkInvalidModalOpen(!isMarkInvalidModalOpen);
 
-  const [isCaseProductInfoCollapsed, setIsCaseProductInfoCollapsed] = useState(false);
-  const handleCaseProductInfoToggle = () =>
-    setIsCaseProductInfoCollapsed(!isCaseProductInfoCollapsed);
+  // const [isCaseProductInfoCollapsed, setIsCaseProductInfoCollapsed] = useState(false);
+  // const handleCaseProductInfoToggle = () =>
+  //   setIsCaseProductInfoCollapsed(!isCaseProductInfoCollapsed);
 
-  const handleNoClick = () => {
-    setIsNoChecked(true);
-    setIsYesChecked(false);
-  };
+  // const handleNoClick = () => {
+  //   setIsNoChecked(true);
+  //   setIsYesChecked(false);
+  // };
 
-  const handleYesClick = () => {
-    setIsNoChecked(false);
-    setIsYesChecked(true);
-  };
+  // const handleYesClick = () => {
+  //   setIsNoChecked(false);
+  //   setIsYesChecked(true);
+  // };
 
-  const [isLockModalOpen, setIsLockModalOpen] = useState(false);
-  const closeLockModal = () => setIsLockModalOpen(false);
-  const toggleLockModal = () => setIsLockModalOpen(!isLockModalOpen);
+  // const [isLockModalOpen, setIsLockModalOpen] = useState(false);
+  // const closeLockModal = () => setIsLockModalOpen(false);
+  // const toggleLockModal = () => setIsLockModalOpen(!isLockModalOpen);
 
-  const [isAddClientModalOpen, setIsAddClientModalOpen] = useState(false)
-  const closeAddClientModal = () => setIsAddClientModalOpen(false);
-  const toggleAddClientModal = () => setIsAddClientModalOpen(!isAddClientModalOpen);
+  // const [isAddClientModalOpen, setIsAddClientModalOpen] = useState(false)
+  // const closeAddClientModal = () => setIsAddClientModalOpen(false);
+  // const toggleAddClientModal = () => setIsAddClientModalOpen(!isAddClientModalOpen);
 
-  const [isAddLeadWarningModalOpen, setIsAddLeadWarningModalOpen] = useState(false)
-  const closeAddLeadWarningModal = () => setIsAddLeadWarningModalOpen(false);
-  const toggleAddLeadWarningModal = () => setIsAddLeadWarningModalOpen(!isAddLeadWarningModalOpen);
+  // const [isAddLeadWarningModalOpen, setIsAddLeadWarningModalOpen] = useState(false)
+  // const closeAddLeadWarningModal = () => setIsAddLeadWarningModalOpen(false);
+  // const toggleAddLeadWarningModal = () => setIsAddLeadWarningModalOpen(!isAddLeadWarningModalOpen);
 
-  const handleChangePage = (p: number) => {
-    setCurrentPage(p);
-  };
+  // const handleChangePage = (p: number) => {
+  //   setCurrentPage(p);
+  // };
 
-  const handleSubmitForAll = (e: any) => {
-    e.preventDefault();
-    setCategory(null);
-    setIsUpdate(true);
-  };
+  // const handleSubmitForAll = (e: any) => {
+  //   e.preventDefault();
+  //   setCategory(null);
+  //   setIsUpdate(true);
+  // };
 
-  const handleSubmitSearch = (e: any) => {
-    e.preventDefault();
-    setIsUpdate(true);
-  };
+  // const handleSubmitSearch = (e: any) => {
+  //   e.preventDefault();
+  //   setIsUpdate(true);
+  // };
 
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimension(window.innerWidth);
-    }
+  // useEffect(() => {
+  //   function handleResize() {
+  //     setWindowDimension(window.innerWidth);
+  //   }
 
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  //   window.addEventListener("resize", handleResize);
+  //   return () => window.removeEventListener("resize", handleResize);
+  // }, []);
 
   return (
-    <StateContext.Provider value={{ state, updateState }}>
+    <StateContext.Provider value={
+      {
+        isDropdownAccountNavBarOpen,
+        closeDropdownAccountNavBarOpen,
+        toggleDropdownAccountNavBar
+      }
+    }>
       {children}
     </StateContext.Provider>
   );
